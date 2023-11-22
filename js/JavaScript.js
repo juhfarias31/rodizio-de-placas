1bac05203af50eb54b1ff69da58711b130720b99
Julia Farias de Queiroz <133894616+juhfarias31@users.noreply.github.com>
2023-11-22 13:58:35 -0300

    Add files via upload

---
+++ js/JavaScript.js
@@ -0,0 +1,28 @@
+function Rodizio(){
+    var texto = document.querySelector("#texto");
+    var placa = document.getElementById("placa").value;
+    var semana = document.getElementById("semana").value;
+
+if ((placa == 1 || placa == 2) && (semana == 1)) {
+    texto.innerHTML = "Não Pode Circular";
+}
+
+else if ((placa == "3" || placa == "4") && (semana == "2")){
+    texto.innerHTML = "Não Pode Circular";
+}
+
+else if ((placa == "5" || placa == "6") && (semana == "3")){
+    texto.innerHTML = "Não Pode Circular";
+}
+
+else if ((placa == "7" || placa == "8") && (semana == "4")) {
+    texto.innerHTML = "Não Pode Circular";
+}
+
+else if ((placa == "9" || placa == "0") && (semana == "5")) {
+    texto.innerHTML = "Não Pode Circular";
+}
+else{
+    texto.innerHTML = "Pode Circular";
+}
+}
